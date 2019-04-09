45308831955d0c082a1cfc60b8e84fb6078e55eb
loriking <lori.king@thoughtworks.com>
2019-04-09 16:06:12 +0200

    Add .gitignore

--- test/com/twu/biblioteca/OptionsMenuTest.java
+++ test/com/twu/biblioteca/OptionsMenuTest.java
@@ -1,4 +1,12 @@
 package com.twu.biblioteca;
 
+import org.junit.Test;
+
 public class OptionsMenuTest {
+
+    @Test
+    public void shouldDisplayListOfOptionsToUser(){
+        OptionsMenu optionsMenu = new OptionsMenu();
+
+    }
 }
