60217f3eebf7877c042b83dae228eb7ccc958a4d
loriking <lori.king@thoughtworks.com>
2019-04-09 08:32:22 +0200

    Create class to contain books

---
+++ src/com/twu/biblioteca/Library.java
@@ -0,0 +1,20 @@
+package com.twu.biblioteca;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List <Book> availableBooks = new ArrayList<Book>();
+
+    public String showAvailableBooks() {
+        String displayBooks = "";
+        for( Book book: availableBooks){
+            displayBooks += book.toString();
+        }
+        return displayBooks;
+    }
+
+    public void addBook(Book book) {
+        availableBooks.add(book);
+    }
+}
