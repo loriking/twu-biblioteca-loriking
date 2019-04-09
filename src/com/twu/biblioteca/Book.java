7f10844a605b4c6432b0ce5f7276e7a9e1c45550
loriking <lori.king@thoughtworks.com>
2019-04-09 14:26:00 +0200

    Add book class

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -1,4 +1,21 @@
 package com.twu.biblioteca;
 
 public class Book {
+    private final String title;
+    private final String author;
+    private final String year;
+    private final String id;
+
+    public Book(String title, String author, String year, String id) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return  title + ", " + author + ", " + year + ", " + id ;
+
+    }
 }
