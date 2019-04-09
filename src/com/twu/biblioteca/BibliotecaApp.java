26f970f30915d477474f497d60bdea7f78c4b009
loriking <lori.king@thoughtworks.com>
2019-04-09 14:18:06 +0200

    Add functionality to get user output

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,30 @@
 package com.twu.biblioteca;
 
+import java.util.Scanner;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BibliotecaMessage messages = new BibliotecaMessage();
+        Library library = new Library();
+
+        Book book1 = new Book("ABC", "Lori", "2019", "1");
+        library.addBook(book1);
+
+        System.out.println(messages.welcomeUser());
+        System.out.println("Select one:");
+        System.out.println(messages.menuOption());
+
+        Scanner userOptions = new Scanner(System.in);
+        String userChoice = userOptions.nextLine();
+
+        String listBooks = messages.menuOption();
+        if(userChoice.equals(listBooks)){
+            System.out.println(library.showAvailableBooks());
+        } else {
+            System.out.println("Please select a valid option.");
+        }
+
+
     }
 }
