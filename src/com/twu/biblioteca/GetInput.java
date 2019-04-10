8ef16e15560a17562c3f35f35fb4f92f0b4f6dcf
loriking <lori.king@thoughtworks.com>
2019-04-10 19:07:59 +0200

    Move customer input to separate class

--- src/com/twu/biblioteca/GetInput.java
+++ src/com/twu/biblioteca/GetInput.java
@@ -1,4 +1,20 @@
 package com.twu.biblioteca;
 
+import java.util.Scanner;
+
 public class GetInput {
+
+    public String getChoice(){
+        Scanner userOptions = new Scanner(System.in);
+        String userChoice = userOptions.nextLine();
+        return userChoice;
+    }
+
+
+
+
+
+
+
+
 }
