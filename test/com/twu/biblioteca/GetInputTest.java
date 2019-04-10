974d18b02da091a626f42d2b52cbad192816c66e
loriking <lori.king@thoughtworks.com>
2019-04-10 19:09:13 +0200

    Add passing tests for GetInput class

--- test/com/twu/biblioteca/GetInputTest.java
+++ test/com/twu/biblioteca/GetInputTest.java
@@ -1,4 +1,17 @@
 package com.twu.biblioteca;
 
+import org.junit.Test;
+
 public class GetInputTest {
+
+
+    @Test
+    public void testGetInput() {
+
+        GetInput choice = new GetInput();
+
+
+
+
+    }
 }
