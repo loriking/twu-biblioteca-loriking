3742af0cefb25b161db19a5f632fc6042369d2a4
loriking <lori.king@thoughtworks.com>
2019-04-11 23:48:52 +0200

    Add methods for borrow and return books

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -4,23 +4,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
-    private List<Book> availableBooks = new ArrayList<Book>();
+    private List<Book> allBooksInLibrary = new ArrayList<Book>();
 
     public Library(){}
 
     public Library(List<Book> books){
-        this.availableBooks = books;
+        this.allBooksInLibrary = books;
+    }
+
+    public String showBooks() {
+        String displayAllBooks = "";
+        for( Book book: allBooksInLibrary){
+                displayAllBooks += "\n";
+                displayAllBooks += book.toString();
+        }
+        return displayAllBooks;
     }
 
     public String showAvailableBooks() {
-        String displayBooks = "";
-        for( Book book: availableBooks){
-            displayBooks += book.toString();
+        String displayAvailableBooks = "";
+        for( Book book: allBooksInLibrary){
+            if (book.checkStatusOfBook()){
+                displayAvailableBooks += "\n";
+                displayAvailableBooks += book.toString();
+            }
         }
-        return displayBooks;
+        return displayAvailableBooks;
     }
 
     public void addBook(Book book) {
-        availableBooks.add(book);
+        allBooksInLibrary.add(book);
+    }
+
+    public String borrowBook(Book book){
+        String getBook = "";
+        if(book.checkStatusOfBook()){
+            book.checkOutBook();
+            getBook = BookLibraryConstants.AVAILABLE;
+        } else {
+            getBook = BookLibraryConstants.BORROWED;
+
+        }
+
+        return getBook;
+    }
+
+
+    public String returnBook(Book book) {
+        boolean libraryContainsBook = allBooksInLibrary.contains(book);
+        if (libraryContainsBook){
+            book.returnBook();
+            return BookLibraryConstants.THANKS;
+        } else {
+            return BookLibraryConstants.WRONG;
+        }
+
+
     }
 }
