2594016745eceb1f5da084dc3fc97f44081989cb
loriking <lori.king@thoughtworks.com>
2019-04-11 23:55:29 +0200

    Add quit case

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -6,14 +6,23 @@ public class BibliotecaApp {
 
     public static void main(String[] args) {
         BibliotecaMessage messages = new BibliotecaMessage();
-        Book book1 = new Book("title", "author", "2019", "1");
+        Book book1 = new Book("title", "author", "2019", true,"1");
+        Book book2 = new Book("title2", "author", "2019", true, "2");
+        Book book3 = new Book("title3", "author", "2019", true, "3");
+        Book book4 = new Book("title4", "author", "2019", true, "4");
+
         ArrayList listOfBooks =  new ArrayList();
         listOfBooks.add(book1);
+        listOfBooks.add(book2);
+        listOfBooks.add(book3);
+        listOfBooks.add(book4);
+
         Library library = new Library(listOfBooks);
         GetInput userInput = new GetInput();
         OptionsMenu options = new OptionsMenu();
 
-        options.addOption("List of books");
+        options.addOption("\t List of books");
+        options.addOption("\t Quit");
 
         System.out.println(messages.welcomeUser()); // Is this a necessary class? Isn't a class overkill?
 
@@ -27,7 +36,7 @@ public class BibliotecaApp {
             userChoice = userInput.getChoice();
             userDidNotQuit = !userChoice.equals("Quit");
             switch(userChoice){
-                case "List":
+                case "List of books":
                     System.out.println(library.showAvailableBooks());
                     break;
                 case "Quit":
