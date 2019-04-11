9f6a0933e86418f6af1dc6bc657aa0a361f83c1f
loriking <lori.king@thoughtworks.com>
2019-04-12 00:01:26 +0200

    Refactor

--- test/com/twu/biblioteca/BibliotecaMessageTest.java
+++ test/com/twu/biblioteca/BibliotecaMessageTest.java
@@ -18,17 +18,8 @@ public class BibliotecaMessageTest {
 
     @Test
     public void shouldReturnWelcomeMessage() {
-        String message1 = "Welcome to Biblioteca. Your one-stop shop for great book titles in Bangalore.";
+        String message1 = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore.";
         assertEquals(message1, message.welcomeUser());
     }
 
-    @Test
-    public void shouldReturnMenuOptionMessage() {
-        String expectedMessage = "List of books";
-        assertEquals(expectedMessage, message.menuOption());
-    }
-
-
 }
-
-//do while loop in java
