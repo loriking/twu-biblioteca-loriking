accb9509c0db98e50bd5d1eea3a00a8fc6d1213a
loriking <lori.king@thoughtworks.com>
2019-04-11 23:39:47 +0200

    Refactor constants into separate class

---
+++ src/com/twu/biblioteca/BookLibraryConstants.java
@@ -0,0 +1,9 @@
+package com.twu.biblioteca;
+
+public class BookLibraryConstants {
+
+    public static final String BORROWED = "Sorry that book is not available";
+    public static final String AVAILABLE = "Book is available";
+    public static final String WRONG = "Sorry that book doesn't belong here";
+    public static final String THANKS = "Thank you for returning book.";
+}
