0ef99c414d6e63ded8a3df978298929da424b172
loriking <lori.king@thoughtworks.com>
2019-04-12 00:35:18 +0200

    Refactor

--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -3,8 +3,6 @@ package com.twu.biblioteca;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-
 import static org.junit.Assert.assertEquals;
 
 public class LibraryTest {
@@ -13,6 +11,7 @@ public class LibraryTest {
     Book book1;
     Book book2;
     Book book3;
+    Book book4;
 
     @Before
     public void setUp(){
@@ -20,17 +19,19 @@ public class LibraryTest {
         book1 = new Book("Jo and her friends", "Jo", "2019", true,"1" );
         book2 = new Book("Mo and his friends", "Mo", "2020", true,"2" );
         book3 = new Book("Po and his friends", "Po", "2020", true,"3" );
+        book4 = new Book("To and his friends", "To", "2020", true,"4" );
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
     }
 
 
     @Test
     public void shouldReturnEmptyStringWhenThereIsNoBookAvailable() {
-        library.addBook(book1);
-        library.addBook(book2);
-        library.addBook(book3);
-        assertEquals("", library.showAvailableBooks());
-    }
+        Library emptyLibrary = new Library();
+        assertEquals("", emptyLibrary.showAvailableBooks());
 
+    }
     @Test
     public void shouldDisplayAllBooks() {
         String expected = "\nJo and her friends\t\t\tJo\t\t\t2019\t\t\t1\nMo and his friends\t\t\tMo\t\t\t2020\t\t\t2" +
@@ -38,21 +39,16 @@ public class LibraryTest {
         assertEquals(expected, library.showBooks());
     }
 
+
     @Test
     public void shouldDisplayABookWhenABookIsAvailable() {
-        library.addBook(book1);
-        library.addBook(book2);
-        library.addBook(book3);
-        book3.checkOutBook();
+        book3.bookCheckedOut();
         String expected = "\nJo and her friends\t\t\tJo\t\t\t2019\t\t\t1\nMo and his friends\t\t\tMo\t\t\t2020\t\t\t2";
         assertEquals(expected, library.showAvailableBooks());
     }
 
     @Test
     public void shouldNotifyIfBookInLibrary() {
-        library.addBook(book1);
-        library.addBook(book2);
-        library.addBook(book3);
         library.borrowBook(book2);
         String expected = "\nJo and her friends\t\t\tJo\t\t\t2019\t\t\t1\nPo and his friends\t\t\tPo\t\t\t2020\t\t\t3";
         assertEquals(expected, library.showAvailableBooks());
@@ -60,9 +56,6 @@ public class LibraryTest {
 
     @Test
     public void shouldNotifyIfBookNotInLibrary(){
-        library.addBook(book1);
-        library.addBook(book2);
-        library.addBook(book3);
         library.borrowBook(book2);
         String expected = BookLibraryConstants.BORROWED;
         String actual = library.borrowBook(book2);
@@ -71,9 +64,6 @@ public class LibraryTest {
 
     @Test
     public void shouldNotifyCustomerOfSucessfulReturn(){
-        library.addBook(book1);
-        library.addBook(book2);
-        library.addBook(book3);
         library.borrowBook(book2);
         String actual = library.returnBook(book2);
         String expected = BookLibraryConstants.THANKS;
@@ -83,7 +73,7 @@ public class LibraryTest {
     @Test
     public void shouldTellCustomerBookIsFromDifferentLibrary(){
         String expected = BookLibraryConstants.WRONG;
-        String actual = library.returnBook(book2);
+        String actual = library.returnBook(book4);
         assertEquals(expected, actual);
 
     }
