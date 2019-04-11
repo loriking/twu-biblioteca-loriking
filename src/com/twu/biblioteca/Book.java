77a5284fb13dbeb719885964c9e6f59d0d371c96
loriking <lori.king@thoughtworks.com>
2019-04-11 23:45:00 +0200

    Add passing test and code for book availability

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -4,18 +4,36 @@ public class Book {
     private final String title;
     private final String author;
     private final String year;
+    private boolean available;
     private final String id;
 
-    public Book(String title, String author, String year, String id) {
+
+    public Book(String title, String author, String year, boolean available, String id) {
         this.title = title;
         this.author = author;
         this.year = year;
+        this.available = available;
         this.id = id;
+    }
+
+    public boolean checkStatusOfBook(){
+        return this.available;
+    }
+
+    public void checkOutBook(){
+        this.available = false;
+        System.out.println("Thank you! Enjoy the book.");
+    }
+
+    public void returnBook(){
+        this.available = true;
+        System.out.println("Thank you for returning the book.");
     }
 
     @Override
     public String toString() {
-        return  title + ", " + author + ", " + year + ", " + id ;
+        String bookFormat = title + "\t\t\t" + author + "\t\t\t" + year + "\t\t\t" + id ;
+        return bookFormat;
 
     }
 }
--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -1,4 +1,23 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class BookTest {
+
+    Book aBook;
+
+    @Before
+    public void setUp(){
+        aBook = new Book("title", "author", "2019", true, "1");
+    }
+
+    @Test
+    public void checkIfBookIsAvailable() {
+        boolean expectedBookStatus = false;
+        aBook.checkOutBook();
+        assertEquals(expectedBookStatus, aBook.checkStatusOfBook());
+    }
 }
